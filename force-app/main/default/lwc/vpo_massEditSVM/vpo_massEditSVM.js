9dc30b493c424096718fc77dbb878671c1a27a06
Thu Phan <phan.thu@aodigy.com>
2022-08-12 17:12:23 +0700

    feature/US-VPO-000028: Added to prevent users from mass editing SVM when Stage is Closed or Cancelled

--- force-app/main/default/lwc/vpo_massEditSVM/vpo_massEditSVM.js
+++ force-app/main/default/lwc/vpo_massEditSVM/vpo_massEditSVM.js
@@ -1,9 +1,14 @@
 import { LightningElement, api, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import { updateRecord } from 'lightning/uiRecordApi';
+import { getRecord } from 'lightning/uiRecordApi';
+
 import getStockVehicleMasters from '@salesforce/apex/VPO_VPOLIController.getStockVehicleMasters';
 
-const columns = [
+import STAGE from '@salesforce/schema/Vehicle_Purchase_Order__c.Stage__c';
+
+const columns = 
+[
     { label: 'RVH Name', fieldName: 'Name', editable: false },
     { label: 'Vehicle Purchase Status', fieldName: 'Vehicle_Purchase_Status__c', editable: false },
     { label: 'Vehicle Status', fieldName: 'Vehicle_Status__c', editable: false },
@@ -17,12 +22,30 @@ export default class Vpo_massEditSVM extends LightningElement
 {
     @api parentId;
     @api navigateToURL;
+    @api showToastNotification;
 
-    draftValues      = [];
-    columns          = columns;
-    rowOffset        = 0;
-    showSuccessToast = false;
-    showErrorToast   = false;
+    draftValues  = [];
+    columns      = columns;
+    rowOffset    = 0;
+    isError      = false;
+    errorMessage = '';
+    stage        = '';
+
+    @wire(getRecord, {recordId: '$parentId', fields: [STAGE]})
+    wiredVPO({error, data})
+    {
+        if (data)
+        {
+            this.stage = data.fields.Stage__c.value;
+
+            if (this.stage === 'Closed' || this.stage === 'Cancelled')
+            {
+                this.isError = true,
+                this.showNotification('Sorry!', 'You cannot edit Stock Vehicle Master when Vehicle Purchase Order Stage is Closed or Cancelled! Please contact your Administrator.', 'warning');
+                this.onClose();
+            }
+        }
+    }
 
     @wire (getStockVehicleMasters, {parentId: '$parentId'}) stockVehicleMasters;
 
@@ -46,28 +69,28 @@ export default class Vpo_massEditSVM extends LightningElement
             await Promise.all(recordUpdatePromises);
 
             // Report success with a toast
-            this.showSuccessToast = true;
-
-            setTimeout(() => {
-                this.showSuccessToast = false;
-            }, 5000 );
+            this.isError = false;
+            this.showNotification('Success!', 'Stock Vehicle Masters have been successfully updated!', 'success');
 
             // Display fresh data in the datatable
             await refreshApex(this.stockVehicleMasters);
         } 
         catch (error) 
         {
-            console.log(error);
-            this.showErrorToast = true;
-
-            setTimeout(() => {
-                this.showErrorToast = false;
-            }, 5000 );
+            console.log(error.body.message);
+            this.isError      = true;
+            this.errorMessage = error.body.message;
+            this.showNotification('Error!', 'An error has occurred! Please contact your Administrator.', 'error');
         }
     }
 
     async onClose()
     {
         await this.navigateToURL('/' + this.parentId);
+    }
+
+    async showNotification(title, message, variant)
+    {
+        await this.showToastNotification(title, message, variant);
     }
 }
