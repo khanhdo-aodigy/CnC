e684d15d41aacc43712abdb17aac8acb0549a208
PhapMaiAodigy <dangphap.mai@aodigy.com>
2022-08-25 21:31:33 +0700

    feature/US-VPO-000041: added VPO Stock Vehicle Master Mass Ref No Update Component

---
+++ force-app/main/default/lwc/vpo_StockVehicleMasterImportExport/vpo_StockVehicleMasterImportExport.js
@@ -0,0 +1,159 @@
+import { LightningElement, track, api, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getStockVehicleMaster from '@salesforce/apex/vpo_StockVehicleMasterIOController.getStockVehicleMaster'
+import updateStockVehicleMaster from '@salesforce/apex/vpo_StockVehicleMasterIOController.updateStockVehicleMaster'
+
+const columns = 
+[
+    { label: 'RVH Name', fieldName: 'Name', editable: false },
+    { label: 'Vehicle Purchase Status', fieldName: 'Vehicle_Purchase_Status__c', editable: false },
+    { label: 'Vehicle Status', fieldName: 'Vehicle_Status__c', editable: false },
+    { label: 'Model Description', fieldName: 'Model_Description__c', editable: false },
+    { label: 'Color Description', fieldName: 'Color_Description__c', editable: false },
+    { label: 'Trim Description', fieldName: 'Trim_Description__c', editable: false },
+    { label: 'Manufacturer Ref. No.', fieldName: 'Manufacturer_Ref_No__c', type: 'text', editable: false },
+];
+
+export default class vpo_StockVehicleMasterImportExport extends LightningElement
+{
+    @api recordId;
+
+    @track csv;
+
+    @track retrievedStockVehicleMasters;
+
+    set csv_(value)
+    {
+        this.csv = value;
+
+        this.csvReady = !!value;
+    }
+
+    get csv_()
+    {
+        return this.csv;
+    }
+
+    @track ready;
+
+    @track csvReady;
+
+    @track enableReportUploader;
+
+    @wire(getStockVehicleMaster, {
+        orderId : '$recordId',
+    })
+    retrivedStockVehicleMasterHandler(result)
+    {
+        this.retrievedStockVehicleMasters = result;
+
+        if (result.error)
+        {
+            console.error(result.error);
+        }
+        else if (result.data)
+        {
+            this.parseToCsvFormat(result.data);
+        }
+
+        this.ready = true;
+    }
+
+    parseToCsvFormat(data)
+    {
+        let csvContent = [];
+
+        let HEADER = [
+            'Id',
+            'Name',
+            'Model_Code__r.Name',
+            'Color_Code__r.ColorCode__c',
+            'Color_Description__c',
+            'Trim_Code__r.Name',
+            'Trim_Description__c',
+            'Manufacturer_Ref_No__c'
+        ];
+
+        csvContent.push(HEADER);
+
+        for (let svm of data)
+        {
+            csvContent.push([
+                svm.Id || '',
+                svm.Name || '',
+                svm.Model_Code__r.Name || '',
+                svm.Color_Code__r.ColorCode__c || '',
+                svm.Color_Description__c || '',
+                svm.Trim_Code__r.Name || '',
+                svm.Trim_Description__c || '',
+                svm.Manufacturer_Ref_No__c || ',',
+            ]);
+        }
+
+        // console.log(csvContent);
+
+        this.generateCsvFile(csvContent);
+    }
+
+    generateCsvFile(rows)
+    {
+        this.csv_ = 'data:text/csv;charset=utf-8,' + rows.map(row => row.join(",")).join("\n");
+    }
+
+    openDownloadWindow(e)
+    {
+        this.template.querySelector('[data-id="hidden-link"]').click();
+    }
+
+    openReportUploader(e)
+    {
+        this.enableReportUploader = true;
+    }
+
+    // @track contentDocumentId_;
+    // @track contentVersionId_;
+
+    handleUploadFinished(e)
+    {
+        const uploadedFiles = e.detail.files;
+
+        this.ready = false;
+
+        updateStockVehicleMaster({
+            contentDocumentId : uploadedFiles[0]['documentId'],
+            contentVersionId : uploadedFiles[0]['contentVersionId']
+        })
+        .then(success => {
+            refreshApex(this.retrievedStockVehicleMasters);
+            this.showNotification_('Success', 'Stock Vehicle Master successfully updated', 'success', 'sticky');
+        })
+        .catch(error => {
+            this.showNotification_('Error', 'Unexpected error on Stock Vehicle Master updating', 'error', 'sticky');
+            console.error(error);
+        })
+        .finally(() => {
+            this.enableReportUploader = false;
+            this.ready = true;
+        })
+    }
+
+    get acceptedFormats_() {
+        return ['.csv'];
+    }
+
+    @track columns_ = columns;
+
+    showNotification_(title, message, variant, mode)
+    {
+        const evt = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            mode: mode
+        });
+
+        this.dispatchEvent(evt);
+    }
+    
+}
